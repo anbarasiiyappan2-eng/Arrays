3ca9197824e37c19d3f7092bf47e201093b5ba9e
anbarasiiyappan2-eng <anbarasiiyappan2@gmail.com>
2026-06-01 16:17:12 +0530

    Add files via upload

---
+++ day 4/rotatearray.java
@@ -0,0 +1,16 @@
+public class rotatearray {
+public static void main(String[] args) {
+int[] arr = {10, 20, 30, 40, 50};
+int last = arr[arr.length - 1];
+
+for (int i = arr.length - 1; i > 0; i--) {
+arr[i] = arr[i - 1];
+}
+
+arr[0] = last;
+
+for (int num : arr) {
+System.out.print(num + " ");
+}
+}
+}
